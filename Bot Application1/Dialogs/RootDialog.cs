9f467ea25faf10df0a6e51c738d569065bc5fe1a
Terrenceteo98 <Terrenceteo98@gmail.com>
2017-04-28 14:51:32 +0800

    add comment test in rootdialog.cs

--- Bot Application1/Dialogs/RootDialog.cs
+++ Bot Application1/Dialogs/RootDialog.cs
@@ -24,7 +24,7 @@ namespace Bot_Application1.Dialogs
 
             // return our reply to the user
             await context.PostAsync($"You sent {activity.Text} which was {length} characters");
-
+            //waiting
             context.Wait(MessageReceivedAsync);
         }
     }
